2c84936ffd8aaf765cac52685127cd91296ed51b
anatoliy2221 <143737752+anatoliy2221@users.noreply.github.com>
2024-01-14 20:57:45 +0000

    Deploying to gh-pages from @ anatoliy2221/goit-js-hw-09@f530cd76711f833c2769ed1b1d765499d3f9148f 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-ec808ebb.js";const e=document.querySelector(".feedback-form");e.addEventListener("input",()=>{const t=e.elements.email.value,a=e.elements.message.value,o="feedback-form-state",s={email:`${t}`,textarea:`${a}`};localStorage.setItem(o,JSON.stringify(s))});
+//# sourceMappingURL=commonHelpers2.js.map
